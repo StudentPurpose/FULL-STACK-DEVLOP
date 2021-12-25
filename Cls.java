300c5f12367bbf45f84e3e200ba2bad5c83ed2e4
MAMIDALA NARESH <mnaresh12@gmail.com>
2021-12-25 21:21:41 +0530

    ok

--- Cls.java
+++ Cls.java
@@ -26,6 +26,12 @@ public class Cls {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 	
 	
 	
